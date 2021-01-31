1eb6d2ef2756d22b8bbef0f515a3e7a36e9970ae
VaibhawKr744 <honeykr744@gmail.com>
2021-01-31 16:50:12 +0530

    Added deck component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Deck from './Deck';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Deck />
     </div>
   );
 }
